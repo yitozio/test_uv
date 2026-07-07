68a705db097d284a3fec8f4ed5f46bf0c0084d55
joey_pc <jackhappystar@163.com>
2026-07-07 16:07:32 +0800

    🐞 fix(src): 修复: 相对引用

--- src/test_uv/__init__.py
+++ src/test_uv/__init__.py
@@ -1,2 +1,11 @@
-from core import aa
+#  ...
+#  @Author: Johnston
+#  @Date: 2026-07-07 16:01:35
+#  @LastEditors: Johnston
+#  @LastEditTime: 2026-07-07 16:07:09
+#  @Description: 
+#  @Reference: 
+#  @Copyright (c) 2026 by Johnston, All Rights Reserved. 
+#  ...
+from .core import aa
 bb = 2
