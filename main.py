f79dacae86eae2c181d3a30fe60346ff997675f3
joey_pc <jackhappystar@163.com>
2026-07-07 15:39:04 +0800

    🎉 init(project): 初始化: 项目

---
+++ main.py
@@ -0,0 +1,21 @@
+#  ...
+#  @Author: Johnston
+#  @Date: 2026-07-07 14:44:30
+#  @LastEditors: Johnston
+#  @LastEditTime: 2026-07-07 15:35:41
+#  @Description:
+#  @Reference:
+#  @Copyright (c) 2026 by Johnston, All Rights Reserved.
+#  ...
+
+
+
+
+def main():
+    print("Hello from test-uv!")
+    import sys
+    print(sys.version)
+
+
+if __name__ == "__main__":
+    main()
