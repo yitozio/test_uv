07e0c2f8a9783438318b38bd265cc6f34f19369e
joey_pc <jackhappystar@163.com>
2026-07-07 16:14:36 +0800

    ✨ feat(src): 增加: init注解

--- src/test_uv/__init__.py
+++ src/test_uv/__init__.py
@@ -2,9 +2,18 @@
 #  @Author: Johnston
 #  @Date: 2026-07-07 16:01:35
 #  @LastEditors: Johnston
-#  @LastEditTime: 2026-07-07 16:07:09
+#  @LastEditTime: 2026-07-07 16:14:08
 #  @Description:
 #  @Reference:
 #  @Copyright (c) 2026 by Johnston, All Rights Reserved.
 #  ...
+
+
+# src/test_uv/__init__.py
+"""test_uv - 你的包描述"""
+
+
 from .core import greet, add_numbers
+
+# __all__ = ["greet", "add_numbers"]
+__version__ = "0.1.0"
