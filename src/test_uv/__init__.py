0dd26befd8c77320ccae7add2f975f3c2cdc242f
joey_pc <jackhappystar@163.com>
2026-07-07 16:11:36 +0800

    ✨ feat(src): 增加: 类型注解函数

--- src/test_uv/__init__.py
+++ src/test_uv/__init__.py
@@ -3,9 +3,8 @@
 #  @Date: 2026-07-07 16:01:35
 #  @LastEditors: Johnston
 #  @LastEditTime: 2026-07-07 16:07:09
-#  @Description: 
-#  @Reference: 
-#  @Copyright (c) 2026 by Johnston, All Rights Reserved. 
+#  @Description:
+#  @Reference:
+#  @Copyright (c) 2026 by Johnston, All Rights Reserved.
 #  ...
-from .core import aa
-bb = 2
+from .core import greet, add_numbers
--- src/test_uv/core.py
+++ src/test_uv/core.py
@@ -1,1 +1,8 @@
-aa = 1
+def greet(name: str = "1") -> str:
+    """一个简单的问候函数"""
+    return f"Hello, {name}!"
+
+
+def add_numbers(a: int = 1, b: int = 2) -> int:
+    """两数相加"""
+    return a + b
